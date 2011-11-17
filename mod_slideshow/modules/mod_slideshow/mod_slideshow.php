d0a0333d339545f16f7fd056273436c4a4888bee
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-17 09:43:10 -0500

    Added slideshow

---
+++ mod_slideshow/modules/mod_slideshow/mod_slideshow.php
@@ -0,0 +1,11 @@
+<?php
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+$image_path = 'images/galleries/'. $params->get('image_path');
+$images = JFolder::files($image_path);
+
+$document = &JFactory::getDocument();
+$document->addStyleSheet('/media/mod_slideshow/css/mod_slideshow.css');
+$document->addScript('/media/mod_slideshow/js/mod_slideshow.js');
+
+require(JModuleHelper::getLayoutPath('mod_slideshow'));
