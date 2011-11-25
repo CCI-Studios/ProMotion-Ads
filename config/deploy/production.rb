c898cba5f5f3d22397ed9b4df1794b97c0563526
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-25 00:49:44 -0500

    Added production server settings

---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+# repository info
+set :branch, "master"
+
+# This may be the same as your `Web` server
+role :app, "pro-motionads.ca"
+
+# directories
+set :deploy_to, "/home/pmads/subdomains/live"
+set :public, "#{deploy_to}/public_html"
+set :extensions, %w[public template com_contracts mod_slideshow]
